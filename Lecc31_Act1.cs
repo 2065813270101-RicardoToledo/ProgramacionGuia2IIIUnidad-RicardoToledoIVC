7097aeff427d7a7977bf75350d1c273f706c5e21
2065813270101-RicardoToledo <r.toledo.5adb@gmail.com>
2026-07-06 22:23:54 -0600

    Add files via upload

---
+++ Lecc31_Act1.cs
@@ -0,0 +1,31 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=============Datos Academicos===============");
+        Console.WriteLine("=============Leccion31_Act1============");
+        Console.WriteLine("=============Nombre: Ricardo Toledo==========");
+        Console.WriteLine("=============Grado Seccion :IV C===============");
+        Console.WriteLine("=============Clave:30=======================");
+        int filas = 3;
+        int columnas = 5;
+        int i = 0;  int j = 0;
+        int[,] matriz1 = new int[3,5];
+
+        for (i = 0; i < filas; i++)  
+        {
+            for (j = 0; j < columnas; j++)
+            {
+                Console.WriteLine("Ingrese el numero que quiere guardar en la fila:" + i+",columna:" + j + "=");
+                matriz1[i,j] = int.Parse(Console.ReadLine());
+            }
+        }
+            for (i=0;i<filas;i++)
+            {
+                for (j=0;j<columnas;j++)
+                {
+                    Console.WriteLine("Fila:" + i + ",columna:" + j + "=" + matriz1[i,j]);
+                }
+            }
+        }
+    }
